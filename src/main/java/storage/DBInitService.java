12b0bd00a15f7f7227d40267dcc0fd8e546bd4d0
Tetyana-Stepanova <95179655+Tetyana-Stepanova@users.noreply.github.com>
2022-08-05 18:15:01 +0300

    Update DBInitService.java

--- src/main/java/storage/DBInitService.java
+++ src/main/java/storage/DBInitService.java
@@ -16,26 +16,7 @@ import java.sql.Statement;
 
 public class DBInitService {
 
-//    private static Connection conn;
-//
-//    public static Connection getConn() {
-//        try {
-//            MysqlDataSource dataSource = new MysqlDataSource();
-//            dataSource.setURL(StorageConstants.CONNECTION_URL);
-//            dataSource.setUser(StorageConstants.USERNAME);
-//            dataSource.setPassword(StorageConstants.PASSWORD);
-//            conn = dataSource.getConnection();
-//
-//        } catch (SQLException e) {
-//            System.out.println("Cannot get connection: " + e);
-//        }
-//        return conn;
-//    }
-
     public void initDb() {
-//       createFromFile("C:\\Users\\Yura\\Desktop\\GoIT\\ProjectManagementSystem\\src\\main\\resources\\db\\migration\\V1__initDB.sql");
-//       createFromFile("C:\\Users\\Yura\\Desktop\\GoIT\\ProjectManagementSystem\\src\\main\\resources\\db\\migration\\V2__populateDB.sql");
-//
         MysqlDataSource dataSource = new MysqlDataSource();
         dataSource.setURL(StorageConstants.CONNECTION_URL);
         dataSource.setUser(StorageConstants.USERNAME);
@@ -44,30 +25,8 @@ public class DBInitService {
         Flyway flyway = Flyway
                 .configure()
                 .dataSource(dataSource)
-//                .locations("filesystem:resources/db/migration/V1__initDB.sql")
                 .load();
 
         flyway.migrate();
     }
-
-//    private void createFromFile(String fileUrl){
-//        java.awt.EventQueue.invokeLater(new Runnable() {
-//            public void run() {
-//                try {
-//                    Statement stmt = getConn().createStatement() ;
-//                    int i = 0 ;
-//                    for(i=1;i<=57;i++) {
-//                        FileReader fr = new FileReader(fileUrl) ;
-//                        BufferedReader br = new BufferedReader(fr) ;
-//                        stmt.execute(br.readLine()) ;
-//                    }
-//                    stmt.close();
-//                    conn.close();
-//                    JOptionPane.showMessageDialog(null, " Records Successfully Inserted into database !", "Success !", 1) ;
-//                } catch(Exception e) {
-//                    JOptionPane.showMessageDialog(null, e, "ERROR", JOptionPane.ERROR_MESSAGE) ;
-//                }
-//            }
-//        });
-//    }
 }
