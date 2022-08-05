5f77a1452f25a07718f36af5fc80afb2734dda09
Tetyana-Stepanova <95179655+Tetyana-Stepanova@users.noreply.github.com>
2022-08-05 18:16:28 +0300

    Update DBInitService.java

--- src/main/java/storage/DBInitService.java
+++ src/main/java/storage/DBInitService.java
@@ -3,17 +3,6 @@ package storage;
 import com.mysql.cj.jdbc.MysqlDataSource;
 import org.flywaydb.core.Flyway;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-import javax.sql.DataSource;
-import javax.swing.*;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
-
 public class DBInitService {
 
     public void initDb() {
